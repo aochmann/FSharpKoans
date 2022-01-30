3a30e575dca9b3c379f46a093d86e8ba4429cf49
Adrian Ochmann <adrian.ochmann@outlook.com>
2022-01-30 18:19:48 +0100

    chore: done

--- FSharpKoans/AboutLooping.fs
+++ FSharpKoans/AboutLooping.fs
@@ -5,8 +5,8 @@ open FSharpKoans.Core
 // About Looping
 //
 // While it's more common in F# to use the Seq, List, or Array
-// modules to perform looping operations, you can still fall 
-// back on traditional imperative looping techniques that you may 
+// modules to perform looping operations, you can still fall
+// back on traditional imperative looping techniques that you may
 // be more familiar with.
 //---------------------------------------------------------------
 [<Koan(Sort = 12)>]
@@ -19,8 +19,12 @@ module ``about looping`` =
         for value in values do
             sum <- sum + value
 
-        AssertEquality sum __
-       
+        let sumRsult =
+            values
+            |> Seq.fold(+) 0 // Aggregation
+
+        AssertEquality sum sumRsult
+
     [<Koan>]
     let LoopingWithExpressions() =
         let mutable sum = 0
@@ -28,16 +32,27 @@ module ``about looping`` =
         for i = 1 to 5 do
             sum <- sum + i
 
-        AssertEquality sum __
+        let sumResult =
+            [|1..5|]
+            |> Seq.fold(+) 0
+
+        AssertEquality sum sumResult
 
     [<Koan>]
     let LoopingWithWhile() =
         let mutable sum = 1
 
+        let rec addUntil (value: int) (maxValue:int) :int=
+            match value with
+            | v when v < maxValue -> addUntil (v + v) maxValue
+            | _ -> value
+
         while sum < 10 do
             sum <- sum + sum
 
-        AssertEquality sum __
+        let result = addUntil 1 10
+
+        AssertEquality sum result
 
     (* NOTE: While these looping constructs can come in handy from time to time,
              it's often better to use a more functional approach for looping
